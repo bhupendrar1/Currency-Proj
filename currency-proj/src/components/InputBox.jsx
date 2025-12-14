81954fe6e1d7eeebb7a7e6b37df6f36c5e30660e
Bhupendra Singh <bs4004908@gmail.com>
2025-12-15 00:10:15 +0530

    currency converter

--- currency-proj/src/components/InputBox.jsx
+++ currency-proj/src/components/InputBox.jsx
@@ -14,7 +14,7 @@ function InputBox({
    const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-sky-500/50 p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId}  className="text-black/40 mb-2
                  inline-block">
@@ -33,7 +33,7 @@ function InputBox({
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    className="rounded-lg px-1 py-1 bg-gray-400 cursor-pointer outline-none"
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
--- currency-proj/src/hooks/useCurrencyInfo.js
+++ currency-proj/src/hooks/useCurrencyInfo.js
@@ -1,18 +1,26 @@
-import { useEffect , useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data , setData] = useState({})
-  useEffect(() => {
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/
-        currency-api@1/latest/currencies/${currency}
-        .json`).then((res) => res.json())
-        .then((res) => setData(res[currency]))
-   console.log(data);
-  }, [currency])
- console.log(data); 
-  return data
-}
+  const [data, setData] = useState({});
 
+  useEffect(() => {
+    fetch(
+      `https://api.currencyapi.com/v3/latest?apikey=cur_live_Ty0LIWQOchG9M3fWh2diG9oPyZr2kXqWrPhtJh88&base_currency=${currency.toUpperCase()}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res?.data) {
+          const rates = {};
+          Object.keys(res.data).forEach((key) => {
+            rates[key.toLowerCase()] = res.data[key].value;
+          });
+          setData(rates);
+        }
+      })
+      .catch((err) => console.error("Currency API Error:", err));
+  }, [currency]);
+
+  return data;
+}
 
 export default useCurrencyInfo;
